9fd876111932319181909aa14d3aba52c42fde28
Tarun <141718214+tarunreturn@users.noreply.github.com>
2024-10-24 14:04:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 provider "aws" {
-  region     = "us-east-1"
+region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0ddc798b3f1a5117e"  # Use a valid AMI ID for your region
-  instance_type = "t2.micro"
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.micro"
+key_name = "Awsdevops"
+vpc_security_group_ids = ["sg-053d3c714ed5a9a87"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
 
-  tags = {
-    Name = "Jenkins-Terraform-EC2"
-  }
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
 }
